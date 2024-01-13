9adbbe525b06604e50e8d2c6886d7147e9005d29
JeongMGyu <alsrb1943@naver.com>
2024-01-13 18:13:47 +0900

    게시판

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Write from "./components/Write";
+import BoardDetail from "./components/BoardDetail";
+import Update from "./components/Update";
+import setProxy from "./setProxy";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} /> // 글 목록, 글 쓰기
+          <Route path="/write" element={<Write />} /> // 글 쓰는 페이지
+          <Route path="/board/:id" element={<BoardDetail />} /> // 글 상세보기
+          <Route path="/update/:id" element={<Update />} /> // 글 수정하기
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ frontend/src/components/Home.js
@@ -0,0 +1,36 @@
+import React, { useEffect } from "react";
+import BoardItem from "./BoardItem";
+import { useState } from "react";
+import axios from "axios";
+
+export default function Home() {
+  const [boards, setBoards] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/board")
+      .then((res) => {
+        setBoards(res.data);
+      })
+      .catch((error) => {
+        console.error("error!", error);
+      });
+  }, []);
+
+  // useEffect(() => {
+  //   fetch("http://localhost:8080/board")
+  //     .then((res) => res.json())
+  //     .then((res) => {
+  //       setBoards(res);
+  //       console.log(1, res);
+  //     });
+  // }, []);
+
+  return (
+    <div>
+      {boards.map((board) => (
+        <BoardItem key={board.id} board={board} />
+      ))}
+    </div>
+  );
+}
