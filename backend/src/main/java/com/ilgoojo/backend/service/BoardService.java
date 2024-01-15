6746b17fd3e8511fa3f18c8c3e8999b743adac98
JeongMGyu <alsrb1943@naver.com>
2024-01-15 15:49:35 +0900

    게시판

--- backend/src/main/java/com/ilgoojo/backend/service/BoardService.java
+++ backend/src/main/java/com/ilgoojo/backend/service/BoardService.java
@@ -2,8 +2,14 @@ package com.ilgoojo.backend.service;
 
 import com.ilgoojo.backend.entity.Board;
 import com.ilgoojo.backend.repository.BoardRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.util.Supplier;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,18 +19,38 @@ import java.util.List;
 @Service
 public class BoardService {
 
-    private final BoardRepository boardRepository;
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @PersistenceContext
+    private EntityManager entityManager;
 
     @Transactional
     public Board boardWrite(Board board) {
         return boardRepository.save(board);
     }
 
-    @Transactional(readOnly = true) //변경감지X
+    @Transactional(readOnly = true)
     public Board boardDetail(Integer id) {
         return boardRepository.findById(id)
                 .orElseThrow(()->new IllegalArgumentException("id를 확인해주세요"));
     }
+
+    @Transactional(readOnly = true)
+    public Page<Board> boardList(Pageable pageable) {
+        return boardRepository.findAll(pageable);
+    }
+
+    public Page<Board> getBoards(int page, int size, String keyword) {
+        return boardRepository.findByKeyword(keyword, PageRequest.of(page, size));
+    }
+
+    @Transactional
+    public void increaseView(Integer id) {
+        Board board = boardRepository.findById(id).get();
+        board.setView(board.getView()+1);
+    }
+
 
     public List<Board> boardList() {
         return boardRepository.findAll();
@@ -36,7 +62,6 @@ public class BoardService {
                 .orElseThrow(()->new IllegalArgumentException("id를 확인해주세요")); //영속화 (Board오브젝트)
         boardEntity.setTitle(board.getTitle());
         boardEntity.setContent(board.getContent());
-
 
         return null;
     }
