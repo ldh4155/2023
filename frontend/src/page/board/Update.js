29940b29b76aab7e3c102d2f8bba3932799c09ca
JeongMGyu <alsrb1943@naver.com>
2024-01-31 17:27:15 +0900

    input 분리

--- frontend/src/page/board/Update.js
+++ frontend/src/page/board/Update.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
-import BoardInput from "./BoardInput";
+import BoardInput from "../../components/BoardInput";
 
 export default function Update(props) {
   const { id } = useParams();
--- frontend/src/page/board/Write.js
+++ frontend/src/page/board/Write.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import BoardInput from "./BoardInput";
+import BoardInput from "../../components/BoardInput";
 
 export default function Write(props) {
   const [board, setBoard] = useState({
@@ -35,6 +35,31 @@ export default function Write(props) {
       });
   }
 
+  //   fetch("http://localhost:8080/board", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json; charset=utf-8",
+  //     },
+  //     body: JSON.stringify(board),
+  //   })
+  //     .then((res) => {
+  //       if (res.status === 201) {
+  //         return res.json();
+  //       } else {
+  //         return null;
+  //       }
+  //     })
+  //     .then((res) => {
+  //       if (res !== null) {
+  //         alert("게시글 작성이 완료 되었습니다.");
+  //         navigate("/");
+  //       } else {
+  //         alert("게시글 등록 실패.");
+  //       }
+  //     });
+  // }
+
+  //input 분리
   return (
     <BoardInput
       SubmitBoard={SubmitBoard}
