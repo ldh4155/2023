7b2436a79afb05f0c2b70af72820912da2d8f459
JeongMGyu <alsrb1943@naver.com>
2024-03-07 15:03:50 +0900

    게시판 파일 업로드(미완)

--- backend/src/main/java/com/ilgoojo/backend/entity/Board.java
+++ backend/src/main/java/com/ilgoojo/backend/entity/Board.java
@@ -4,21 +4,31 @@ import jakarta.persistence.*;
 import lombok.Data;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Entity
 @Data
 @EntityListeners(AuditingEntityListener.class)
-public class Board extends BaseTime{
+public class Board extends BaseTime {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
     private String title;
     private String content;
+    private String filename;
+    private String filepath;
+
+
     @Column(columnDefinition = "integer default 0", nullable = false)
     private int view;
 
     @ManyToOne
-    @JoinColumn(name="member_id")
+    @JoinColumn(name = "member_id")
     private Member member;
 
+
 }
+
+
