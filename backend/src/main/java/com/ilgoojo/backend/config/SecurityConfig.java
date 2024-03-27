a9841f09ccf5676b7f59beedfcdda785ab68298b
ldh4155 <cdf4133@gmail.com>
2024-03-27 22:05:55 +0900

    403에러 수정

--- backend/src/main/java/com/ilgoojo/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/ilgoojo/backend/config/SecurityConfig.java
@@ -71,7 +71,7 @@ public class SecurityConfig {
         http.httpBasic((auth) -> auth.disable());
         //어느 api는 인가 없이 들어올지
         http.authorizeHttpRequests((auth) -> auth
-                .requestMatchers("/signin", "signup", "/").permitAll()
+                .requestMatchers("/login", "/signup", "/","/images/**").permitAll()
                 .requestMatchers(HttpMethod.GET, "/board/**").permitAll()
                 .anyRequest().authenticated());
 
