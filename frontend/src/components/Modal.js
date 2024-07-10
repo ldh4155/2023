896d55e5a579e3c183c98a876ce748d85352a263
ldh4155 <cdf4133@gmail.com>
2024-07-10 16:09:51 +0900

    게시판 모달 이동

--- frontend/src/components/Modal.js
+++ frontend/src/components/Modal.js
@@ -1,46 +1,40 @@
 import { useState } from "react";
 import ModalBoard from "../page/board/ModalBoard";
 import styles from '../style/cssmodule/components/Modal.module.css';
-import { Toast, ToastContainer } from 'react-bootstrap';
+import { Modal } from 'react-bootstrap';
 import Draggable from "react-draggable";
 
 function ToastNotification({ boardData }) {
-    const [showToast, setShowToast] = useState(false);
-    const [toastBoard, setToastBoard] = useState(boardData);
+    const [showModal, setShowModal] = useState(false);
+    const [modalBoard, setModalBoard] = useState(boardData);
 
-    function toggleToast() {
-        setShowToast(!showToast);
-        if (!showToast) {
-            setToastBoard(boardData);
+    function toggleModal() {
+        setShowModal(!showModal);
+        if (!showModal) {
+            setModalBoard(boardData);
         }
     }
 
     return (
         <div>
-            <button onClick={toggleToast} className={styles.buttonStyle}>
-                작은 창으로 보기
+            <button onClick={toggleModal} className={styles.buttonStyle}>
+                모달로 보기
             </button>
             <Draggable>
-                <div>
-                    <ToastContainer
-                        position="top-end"
-                        className={`p-3 ${styles.customToast}`}
-                    >
-                        <Toast
-                            show={showToast}
-                            onClose={() => setShowToast(false)}
-                            autohide
-                            className={styles.customDialog}
-                        >
-                            <Toast.Header className={styles.ToastHeaderCustom}>
-                                <strong className="me-auto">게시글</strong>
-                            </Toast.Header>
-                            <Toast.Body className={styles.ToastBodyCustom}>
-                                <ModalBoard boardData={toastBoard} />
-                            </Toast.Body>
-                        </Toast>
-                    </ToastContainer>
-                </div>
+                <Modal
+                    show={showModal}
+                    onHide={() => setShowModal(false)}
+                    backdrop="static"
+                    keyboard={false}
+                    className={styles.customDialog}
+                >
+                    <Modal.Header closeButton className={styles.ToastHeaderCustom}>
+                        <Modal.Title>게시글</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body className={styles.ToastBodyCustom}>
+                        <ModalBoard boardData={modalBoard} />
+                    </Modal.Body>
+                </Modal>
             </Draggable>
         </div>
     );
