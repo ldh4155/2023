cba9aa81f19b4fcbb5a3a7d5061c509607c82f20
ldh4155 <cdf4133@gmail.com>
2024-07-11 21:36:12 +0900

    css 오류 수정

--- frontend/src/page/board/ModalBoard.js
+++ frontend/src/page/board/ModalBoard.js
@@ -1,5 +1,5 @@
 import ImageList from "../../components/ImageList";
-import styles from '../../style/cssmodule/Board/ModalBasic.moddule.css';
+import styles from '../../style/cssmodule/Board/ModalBasic.module.css';
 
 
 
