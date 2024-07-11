00944a2e97032bf7d8d6aefccbf1d902f8371ec9
ldh4155 <cdf4133@gmail.com>
2024-07-11 16:02:53 +0900

    게시글 모달 수정

--- frontend/src/page/board/ModalBoard.js
+++ frontend/src/page/board/ModalBoard.js
@@ -1,20 +1,26 @@
 import ImageList from "../../components/ImageList";
-import styles from '../../style/cssmodule/Board/modalBoard.module.css';
+import styles from '../../style/cssmodule/Board/ModalBasic.module.css'
 
-function ModalBoard({ boardData, setIsModal, setModalBoard }) {
+
+
+function ModalBoard({ boardData, togglModal }) {
   return (
-      <div className={styles.container}>
-        {boardData ? (
-            <div className={styles.modalContent}>
-              <h1 className={styles.title}>제목 : {boardData.title}</h1>
-              <div className={styles.hr}></div>
-              <h3 className={styles.content}>내용 : {boardData.content}</h3>
-              <ImageList imageUrls={boardData.imageUrls} />
-            </div>
-        ) : (
-            <></>
-        )}
-      </div>
+    <div className={styles.container}>
+      {boardData ? (
+        <div>
+          <button className={styles.close} onClick={togglModal}>
+            {" "}
+            x{" "}
+          </button>
+          <h1>제목 : {boardData.title}</h1>
+          <hr />
+          <h3>내용 : {boardData.content}</h3>
+          <ImageList imageUrls={boardData.imageUrls} />
+        </div>
+      ) : (
+        <></>
+      )}
+    </div>
   );
 }
 export default ModalBoard;
