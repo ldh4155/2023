ae3cc6ffe557da915e35ed788db0cc27d911564f
JeongMGyu <alsrb1943@naver.com>
2024-07-11 13:15:12 +0900

    모달 수정

--- frontend/src/components/Modal.js
+++ frontend/src/components/Modal.js
@@ -1,30 +1,45 @@
 import { useState } from "react";
 import ModalBoard from "../page/board/ModalBoard";
+import styles from "../style/cssmodule/components/Modal.module.css";
+import { Modal } from "react-bootstrap";
 import Draggable from "react-draggable";
-import BoardList from "../page/board/BoardList";
 
-function Modal({ boardData }) {
-  const [isModal, setIsModal] = useState(false);
+function ToastNotification({ boardData }) {
+  const [showModal, setShowModal] = useState(false);
   const [modalBoard, setModalBoard] = useState(boardData);
 
   function toggleModal() {
-    setIsModal(!isModal);
-    if (isModal) {
+    setShowModal(!showModal);
+    if (!showModal) {
       setModalBoard(boardData);
     }
   }
 
   return (
     <div>
-      <button onClick={toggleModal}>모달</button>
-      {isModal === true ? (
+      <button onClick={toggleModal} className={styles.buttonStyle}>
+        모달로 보기
+      </button>
+      {showModal === true ? (
         <Draggable>
-          <div>
-            <ModalBoard boardData={modalBoard} />
-          </div>
+          <Modal
+            show={showModal}
+            onHide={() => setShowModal(false)}
+            backdrop="static"
+            keyboard={false}
+            className={styles.customDialog}
+          >
+            <Modal.Header closeButton className={styles.ToastHeaderCustom}>
+              <Modal.Title>게시글</Modal.Title>
+            </Modal.Header>
+            <Modal.Body className={styles.ToastBodyCustom}>
+              <ModalBoard boardData={modalBoard} />
+            </Modal.Body>
+          </Modal>
         </Draggable>
       ) : null}
     </div>
   );
 }
-export default Modal;
+
+export default ToastNotification;
